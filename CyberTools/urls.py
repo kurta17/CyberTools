7cc0300995d9bb18bb08d4dec6edd2ef71ff1760
Giorgi Kurtanidze <thinkers.struggle@gmail.com>
2024-09-22 14:33:07 +0700

    working with URLS

---
+++ CyberTools/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from tools import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('password-checker/', views.password_checker, name='password_checker'),
+    path('network-scanner/', views.network_scanner, name='network_scanner'),
+    path('encryption-tool/', views.encryption_tool, name='encryption_tool'),
+
+
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # path('register/', views.register, name='register'),
+]
